0ecd1a637c8f1825e065c7c4e79baafec7f6730a
tywil04 <68185249+tywil04@users.noreply.github.com>
2023-02-22 09:43:37 +0000

    fix email 2fa challenge requiring auth

---
+++ ui/src/lib/components/dialog/WebauthnDialog.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { enhance } from "$app/forms";
+
+    import * as webauthnJson from "@github/webauthn-json/browser-ponyfill"
+
+    import * as storage from "$lib/js/storage.js"
+
+    import TextInput from "$lib/components/inputs/TextInput.svelte"
+
+    let submitError
+
+    async function webauthnRegisterChallenge({ data, cancel }) {
+        submitError = undefined
+
+        const name = data.get("name")
+        const authToken = await storage.getAuthToken()
+
+        const response = await fetch(`/api/v1/2fa/webauthn/register`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": authToken,
+            },
+        })
+        const json = await response.json()
+
+        if (response.status !== 200) {
+            submitError = json.error.code
+        } else if (response.status === 200) {
+            const webauthnChallengeId = json.webauthnRegisterChallengeId
+            const credential = await webauthnJson.create(webauthnJson.parseCreationOptionsFromJSON(json.options))
+
+            const finishResponse = await fetch("/api/v1/2fa/webauthn/register", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": authToken,
+                },
+                body: JSON.stringify({
+                    webauthnRegisterChallengeId: webauthnChallengeId,
+                    name: name,
+                    credential: credential,
+                })
+            })
+            const finishJson = await finishResponse.json()
+
+            if (finishResponse.status !== 200) {
+                submitError = finishJson.error.code
+            } else if (finishResponse.status === 200) {
+                console.log(finishJson)
+                console.log("reigsted")
+            }
+        }
+
+        cancel()
+    }
+</script>
+
+<dialog>
+    <form method="POST" use:enhance={webauthnRegisterChallenge}>
+        <TextInput class="flex-grow" label="Name" name="name" description="Enter a namae."/>
+        <button type="submit">start</button>
+    </form>
+</dialog>
